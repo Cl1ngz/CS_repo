0596980f0e0f5f3f7e3efb2ce8c721cfe49a25f8
Cl1ngz <ByteIgma@gmail.com>
2025-03-18 16:45:03 +0100

    Stop tracking /target directory

--- something/target/debug/deps/something-9aec5d1ca6adadf7.d
+++
@@ -1,5 +0,0 @@
-/home/cl1ngz/Documents/CS_repo/something/target/debug/deps/libsomething-9aec5d1ca6adadf7.rmeta: src/main.rs
-
-/home/cl1ngz/Documents/CS_repo/something/target/debug/deps/something-9aec5d1ca6adadf7.d: src/main.rs
-
-src/main.rs:
--- something/target/debug/deps/something-de083ae495e49423.d
+++
@@ -1,5 +0,0 @@
-/home/cl1ngz/Documents/CS_repo/something/target/debug/deps/something-de083ae495e49423: src/main.rs
-
-/home/cl1ngz/Documents/CS_repo/something/target/debug/deps/something-de083ae495e49423.d: src/main.rs
-
-src/main.rs:
